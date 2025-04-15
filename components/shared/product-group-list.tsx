12bc650c7a58d3b62953639a6584a592a96b8104
Evgeniy Datskij <evgenij.datskij@mail.ru>
2025-04-15 22:15:22 +0500

    create ProductGroup

--- components/shared/product-group-list.tsx
+++ components/shared/product-group-list.tsx
@@ -1,9 +1,37 @@
+import { cn } from '@/lib/utils';
+import { ProductCart, Title } from './index';
 import React from 'react';
 
 interface Props {
+  title: string;
+  items: any[];
+  categoryId: number;
+  listClassName?: string;
   className?: string;
 }
 
-export const ProductGroupList: React.FC<Props> = ({ className }) => {
-  return <div className={className}></div>;
+export const ProductGroupList: React.FC<Props> = ({
+  title,
+  items,
+  categoryId,
+  listClassName,
+  className,
+}) => {
+  return (
+    <div className={className}>
+      <Title text={title} size="lg" className="font-extrabold mb-5" />
+      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+        {items.map((product, i) => (
+          <ProductCart
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            // price={product.item[0]}
+            imageUrl={product.imageUrl}
+            key={product.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
